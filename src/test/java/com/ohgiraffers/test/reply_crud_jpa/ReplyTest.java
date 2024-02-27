4aa93f73c148a4e8ed991a793e23d28fa48299d7
dongh810 <105986200+dongh810@users.noreply.github.com>
2024-02-27 23:47:59 +0900

    jpa추가

---
+++ src/test/java/com/ohgiraffers/test/reply_crud_jpa/ReplyTest.java
@@ -0,0 +1,43 @@
+package com.ohgiraffers.test.reply_crud_jpa;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class ReplyTest {
+
+    private static EntityManagerFactory entityManagerFactory;
+
+    private EntityManager entityManager;
+
+    @BeforeAll
+    public static void initFactory() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("jpatest");
+    }
+
+    @BeforeEach
+    public void initManager() {
+        entityManager = entityManagerFactory.createEntityManager();
+    }
+
+    @AfterAll
+    public static void closeFactory() {
+        entityManagerFactory.close();
+    }
+
+    @AfterEach
+    public void closeManager() {
+        entityManager.close();
+    }
+
+    @Test
+    public void JPA_조회_테스트() {
+        Reply foundReply = entityManager.find(Reply.class,1);
+
+        assertTrue(foundReply != null);
+    }
+
+}
