8ea6d50f0292640b66fcdef81ffaa6e94d6e8ef6
dongh810 <105986200+dongh810@users.noreply.github.com>
2024-03-01 22:48:41 +0900

    각 기능별 컨트롤러 추가

---
+++ src/main/java/com/ohgiraffers/test/reply/ReplyController.java
@@ -0,0 +1,39 @@
+package com.ohgiraffers.test.reply;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/reply")
+public class ReplyController {
+
+    ReplyService replyService;
+
+    @Autowired
+    public ReplyController(ReplyService replyService) {
+        this.replyService = replyService;
+    }
+
+    @GetMapping("/member/{memberId}")
+    public List<Reply> selectReplyByWriter(@PathVariable int memberId) {
+        return replyService.selectReplyByWriter(memberId);
+    }
+
+    @GetMapping("/board/replies/{boardId}")
+    public List<Reply> selectAllReplyInBoard(@PathVariable int boardId) {
+        return replyService.selectAllReplyInBoard(boardId);
+    }
+
+    @GetMapping("/board/search/{inputValue}")
+    public List<Reply> selectReplyBySearch(@PathVariable String inputValue) {
+        return replyService.selectReplyBySearch(inputValue);
+    }
+
+
+
+}
